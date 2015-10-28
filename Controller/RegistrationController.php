689135a1ff4d7ea8c5a7cdf2cd7d4e139863df19
Vivien SDW <vivien@sentinellesduweb.com>
2015-10-28 09:27:29 +0100

    fixing RegistrationController avoiding Exception when trying to render register form

--- Controller/RegistrationController.php
+++ Controller/RegistrationController.php
@@ -3,19 +3,32 @@
 namespace Librinfo\UserBundle\Controller;
 
 use Sonata\UserBundle\Controller\RegistrationFOSUser1Controller as SonataRegistrationController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-class RegistrationController extends SonataRegistrationController {
-    public function registerAction() {
+class RegistrationController extends SonataRegistrationController
+{
+    public function registerAction()
+    {
+        if ($this->container->get('request')->getPathInfo() == "/_fragment")
+            return new Response('', 200);
+        else
+            throw new NotFoundHttpException();
+    }
+
+    public function checkEmailAction()
+    {
         throw new NotFoundHttpException();
     }
-    public function checkEmailAction() {
+
+    public function confirmAction($token)
+    {
         throw new NotFoundHttpException();
     }
-    public function confirmAction($token) {
-        throw new NotFoundHttpException();
-    }
-    public function confirmedAction() {
+
+    public function confirmedAction()
+    {
         throw new NotFoundHttpException();
     }
 }
